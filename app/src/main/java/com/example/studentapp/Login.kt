94548b0ee1b0673998f03f322ac24604d5fe9a3b
Jatin Nandal <2017ucs0042@iitjammu.ac.in>
2020-10-14 18:45:14 +0530

    Initial commit- Login and Register Activity

---
+++ app/src/main/java/com/example/studentapp/Login.kt
@@ -0,0 +1,11 @@
+package com.example.studentapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
