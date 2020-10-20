59ccd8758209a727e1bd3f0413b96e4daa4a47fb
Supercool1604 <kabirjuneja007@gmail.com>
2020-10-20 18:12:59 +0530

    TimeTable Activity, need to add logout functionality

--- app/src/main/java/com/example/studentapp/MainActivity.kt
+++ app/src/main/java/com/example/studentapp/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.example.studentapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fauth: FirebaseAuth = FirebaseAuth.getInstance()
+        val mLogoutBtn: Button = findViewById(R.id.mainlogout)
+        timetable.setOnClickListener()
+        {
+            startActivity(Intent(this, TimeTable::class.java))
+        }
+        
+
     }
 
 
+
+
+
 }
+
